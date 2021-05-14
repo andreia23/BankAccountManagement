48767bc7e53132ffc6049de3428e0cc377f790bd
andreia23 <andreialima.berto08@gmail.com>
2021-05-14 01:04:32 -0300

    first commit

---
+++ src/test/java/com/donus/challenge/api/account/management/BankAccountManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donus.challenge.api.account.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
