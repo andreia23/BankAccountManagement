fd64bcc4e7c95053cae9435911397fbeb4210a47
andreia23 <andreialima.berto08@gmail.com>
2021-05-23 11:16:07 -0300

    fix: ajustando swagger

--- src/main/java/com/donus/challenge/api/account/management/SwaggerConfig.java
+++ src/main/java/com/donus/challenge/api/account/management/SwaggerConfig.java
@@ -1,11 +1,11 @@
 package com.donus.challenge.api.account.management;
 
-import static springfox.documentation.builders.PathSelectors.regex;
-
 import java.util.ArrayList;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -15,33 +15,34 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
-	
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+
 	@Bean
-    	public Docket productApi() {
-        	return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.donus.challenge.api.account.management"))
-                .paths(regex("/banking-system.*"))
-                .build()
-                .apiInfo(metaInfo());
-    	}
+	public Docket greetingApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.donus.challenge.api.account.management")).build()
+				.apiInfo(metaData());
 
-    	private ApiInfo metaInfo() {
-		ApiInfo apiInfo = new ApiInfo(
-                "Grenciamento de contas bancárias",
-                "API REST com algumas funções essenciais relacionadas ao gerenciamento de contas bancárias",
-                "1.0.0",
-                "Terms of Service",
-                new Contact("Andréia Berto", "https://github.com/andreia23/BankAccountManagement",
-                            "andreialima.berto08@gmail.com"),
-                "Apache License Version 2.0",
-                "https://www.apache.org/licesen.html", new ArrayList<VendorExtension>()
-        );
+	}
 
-        return apiInfo;
-    }
+	private ApiInfo metaData() {
+		ApiInfo apiInfo = new ApiInfo("Gerenciamento de contas bancárias",
+				"API REST com algumas funções essenciais relacionadas ao gerenciamento de contas bancárias", "1.0.0",
+				"Terms of Service",
+				new Contact("Andréia Berto", "https://github.com/andreia23/BankAccountManagement",
+						"andreialima.berto08@gmail.com"),
+				"Apache License Version 2.0", "https://www.apache.org/licesen.html", new ArrayList<VendorExtension>());
+		return apiInfo;
+
+	}
+
+	@Override
+	protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+
 }
